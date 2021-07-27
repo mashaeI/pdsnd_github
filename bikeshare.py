35783da40c9b34bc295212434fe39731a774d52a
mashael <meshae.q@gmail.com>
2021-07-27 21:10:09 +0300

    Delete comment from bikeshare file

--- bikeshare.py
+++ bikeshare.py
@@ -11,8 +11,6 @@ months = ['january', 'february', 'march', 'april', 'may', 'june','all']
 days = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday','all']
 
 
-# In[3]:
-
 
 def get_filters():
     """
@@ -25,7 +23,7 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     while True:
-        
+
             city_input = input("Please choose city name from this list(chicago , new york city , washington): ").lower()
             if city_input in CITY_DATA:
                 city = CITY_DATA[city_input]
@@ -34,17 +32,17 @@ def get_filters():
 
             month_input = input("Please choose month from this list(january , february , march , april , may , june ,or all): ").lower()
             if month_input in months:
-                month = month_input                
-            while month_input not in months: 
+                month = month_input
+            while month_input not in months:
                 month = input ('\n Invalid answer please choose one from this list (january , february , march , april , may , june ,or all)').lower()
-            
+
             day_input = input("Please choose day from this list(monday , tuesday , wednesday , thursday , friday , saturday , sunday , or all): ").lower()
             if day_input in days:
                 day = day_input
-            while day_input not in days: 
-                day = input ('\n Invalid answer please choose one from this list(monday , tuesday , wednesday , thursday , friday , saturday , sunday , or all)') 
-                
-                
+            while day_input not in days:
+                day = input ('\n Invalid answer please choose one from this list(monday , tuesday , wednesday , thursday , friday , saturday , sunday , or all)')
+
+
             print("You choose " , city, month, day)
             confairming = input(" (yes or no) :").lower()
             if confairming  == 'yes' :
@@ -75,33 +73,33 @@ def load_data(city, month, day):
     df['month'] = df['Start Time'].dt.month
     df['day_of_week'] = df['Start Time'].dt.day_name()
     df['start_hour'] = df['Start Time'].dt.hour
-    
+
     if month != 'all':
         month = months.index(month) +1
         df = df[df['month'] == month]
-    
+
     if day != 'all':
         df = df[df['day_of_week'] == day.title()]
-    
+
     return df
 
 
 
 def time_stats(df):
     """Displays statistics on the most frequent times of travel."""
-    
+
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
 
     common_month = df['month'].mode()[0].astype(str)
-    print("The most common month is : " , common_month.title())  
-  
-    common_day_of_week = df['day_of_week'].mode()[0] 
+    print("The most common month is : " , common_month.title())
+
+    common_day_of_week = df['day_of_week'].mode()[0]
     print("The most common day of week is : " , common_day_of_week.title())
-    
-    common_start_hour = df['start_hour'].mode()[0]  
+
+    common_start_hour = df['start_hour'].mode()[0]
     print("The most common start hour is : " , common_start_hour)
-    
+
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
@@ -116,15 +114,15 @@ def station_stats(df):
 
     start = df['Start Station'].mode()[0]
     print("The most commonly used start station is: " , start)
-    
+
     end = df['End Station'].mode()[0]
     print("The most commonly used end station is: " , end)
 
-    df['combination'] = 'From ' + df['Start Station'] + ' to ' + df['End Station'] 
+    df['combination'] = 'From ' + df['Start Station'] + ' to ' + df['End Station']
     combination = df['combination'].mode()[0]
     print("The most frequent combination of start station and end station trip is: " , combination)
 
-    
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
@@ -161,25 +159,25 @@ def user_stats(df):
 
         gender = df['Gender'].value_counts()
         print("The count of gender is: " , gender)
-    
+
     except:
         print('there is no gender data')
         pass
-    
+
     try:
         earliest_birth = df['Birth Year'].min()
         print("Earliest birth from the given fitered data is: " , earliest_birth)
-        
+
         most_recent_birth = df['Birth Year'].max()
         print("Most recent birth from the given fitered data is: " , most_recent_birth)
-   
+
         most_common_birth = df['Birth Year'].mode()[0]
         print("Most common birth from the given fitered data is: ", most_common_birth)
     except:
         print('there is no birth data')
         pass
-    
-    
+
+
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
 
@@ -187,7 +185,7 @@ def user_stats(df):
 
 def display_raw_data(df):
     """View 5 lines of raw data."""
-    
+
     i = 0
     raw_input = input("Would you like to view next five row of raw data? Enter yes or no ").lower()
     while True:
@@ -218,13 +216,13 @@ def main():
         trip_duration_stats(df)
         user_stats(df)
         display_raw_data(df)
-        
+
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             print("\nExplore US bike share data is done. ")
             break
 
 
-            
+
 if __name__ == "__main__":
 	main()
